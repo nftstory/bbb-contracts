1231b7f24bbf6db3f09d2bd97ffbe25692b3fc99
nicholas <nnnnicholas@users.noreply.github.com>
2023-12-28 23:07:16 -0500

    Commented out deploy / tests to allow it to build

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -8,6 +8,6 @@ import { BaseScript } from "./Base.s.sol";
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract Deploy is BaseScript {
     function run() public broadcast returns (BBB bbb) {
-        bbb = new BBB();
+        // bbb = new BBB();
     }
 }
