fcf048c0b0251985963dfed58d211fdfb3cea889
nicholas <nnnnicholas@users.noreply.github.com>
2024-01-08 16:47:08 -0500

    removed useless commented code

--- test/BBB.t.sol
+++ test/BBB.t.sol
@@ -149,96 +149,13 @@ contract BBBTest is StdCheats, Test {
 
         (, address msgSender, address txOrigin) = vm.readCallers();
         test_mint_with_intent(); // mint with intent to issue tokenId 1
+
         vm.startPrank(buyer, buyer);
         bbb.mint(1, 1);
         vm.stopPrank();
+        
         assertEq(bbb.balanceOf(buyer, 1), amount + 1);
     }
-
-    /**
-     * function test_mintWithIntent() external {
-     *     // MintIntent memory intent = MintIntent({
-     *     //     creator: creator, // The creator fee beneficiary
-     *     //     signer: signer, // The "large blob" signer
-     *     //     priceModel: priceModel, // The price curve
-     *     //     uri: uri // The ipfs metadata digest
-     *     //  });
-     *
-     *     // (, string memory name, string memory version, uint256 chainId, address verifyingContract,,) =
-     *     // bbb.eip712Domain(); // Fetch domain from the contract
-     *
-     *     string[] memory inputs = new string[](7);
-     *     inputs[0] = "node test/js-helpers/signData.js";
-     *     inputs[1] = name;
-     *     inputs[2] = version;
-     *     inputs[3] = vm.toString(block.chainid);
-     *     inputs[4] = vm.toString(address(bbb));
-     *     inputs[5] = vm.toString(creator);
-     *     inputs[6] = vm.toString(priceModel);
-     *
-     *     // Call signData.js (via FFI) and pass BBB constructor args to generate the signature we'll verify in the
-     * next
-     *     // step
-     *     bytes memory signature = vm.ffi(inputs);
-     *
-     *     // Extract v, r, s components from the signature
-     *     bytes32 r;
-     *     bytes32 s;
-     *     uint8 v;
-     *     assembly {
-     *         r := mload(add(signature, 32))
-     *         s := mload(add(signature, 64))
-     *         v := byte(0, mload(add(signature, 96)))
-     *     }
-     *
-     *     // MintIntent struct data
-     *     MintIntent memory intent = MintIntent({
-     *         creator: creator,
-     *         signer: signer, // TODO confirm this is right value to pass
-     *         priceModel: priceModel,
-     *         uri: uri
-     *     });
-     *
-     *     // Call the mintWithIntent function
-     *     bbb.mintWithIntent(intent, 1, v, r, s);
-     *
-     *     // Assertions to verify the test results
-     * }
-     */
-
-    // /// @dev Basic test. Run it with `forge test -vvv` to see the console log.
-    // function test_Example() external {
-    //     console2.log("Hello World");
-    //     uint256 x = 42;
-    //     assertEq(foo.id(x), x, "value mismatch");
-    // }
-
-    // /// @dev Fuzz test that provides random values for an unsigned integer, but which rejects zero as an input.
-    // /// If you need more sophisticated input validation, you should use the `bound` utility instead.
-    // /// See https://twitter.com/PaulRBerg/status/1622558791685242880
-    // function testFuzz_Example(uint256 x) external {
-    //     vm.assume(x != 0); // or x = bound(x, 1, 100)
-    //     assertEq(foo.id(x), x, "value mismatch");
-    // }
-
-    // /// @dev Fork test that runs against an Ethereum Mainnet fork. For this to work, you need to set
-    // `API_KEY_ALCHEMY`
-    // /// in your environment You can get an API key for free at https://alchemy.com.
-    // function testFork_Example() external {
-    //     // Silently pass this test if there is no API key.
-    //     string memory alchemyApiKey = vm.envOr("API_KEY_ALCHEMY", string(""));
-    //     if (bytes(alchemyApiKey).length == 0) {
-    //         return;
-    //     }
-
-    //     // Otherwise, run the test against the mainnet fork.
-    //     vm.createSelectFork({ urlOrAlias: "mainnet", blockNumber: 16_428_000 });
-    //     address usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    //     address holder = 0x7713974908Be4BEd47172370115e8b1219F4A5f0;
-    //     uint256 actualBalance = IERC20(usdc).balanceOf(holder);
-    //     uint256 expectedBalance = 196_307_713.810457e6;
-    //     assertEq(actualBalance, expectedBalance);
-    // }
 
     /*//////////////////////////////////////////////////////////////
                             HELPER FUNCTIONS
@@ -277,42 +194,4 @@ contract BBBTest is StdCheats, Test {
         return (v, r, s, digest);
     }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual returns (bool) {
-        return interfaceId == bytes4(0xf23a6e61) || interfaceId == bytes4(0xbc197c81); // ERC1155Receiver
-    }
-
-    // function onERC1155Received(
-    //     address operator,
-    //     address from,
-    //     uint256 id,
-    //     uint256 value,
-    //     bytes calldata data
-    // ) external returns (bytes4);
-
-    /**
-     * @dev Handles the receipt of a multiple ERC1155 token types. This function
-     * is called at the end of a `safeBatchTransferFrom` after the balances have
-     * been updated.
-     *
-     * NOTE: To accept the transfer(s), this must return
-     * `bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))`
-     * (i.e. 0xbc197c81, or its own function selector).
-     *
-     * @param operator The address which initiated the batch transfer (i.e. msg.sender)
-     * @param from The address which previously owned the token
-     * @param ids An array containing ids of each token being transferred (order and length must match values array)
-     * @param values An array containing amounts of each token being transferred (order and length must match ids array)
-     * @param data Additional data with no specified format
-     * @return `bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))` if transfer is
-     * allowed
-     */
-    // function onERC1155BatchReceived(
-    //     address operator,
-    //     address from,
-    //     uint256[] calldata ids,
-    //     uint256[] calldata values,
-    //     bytes calldata data
-    // ) external returns (bytes4){
-    //     return bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"));
-    // }
 }
