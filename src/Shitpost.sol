193ac4a415ce91ef0fbd0330aa0dfa5ce5a2b162
Ape Toshi <admin@apetoshi.com>
2024-03-30 14:15:49 +0100

    Add changeFeeRecipient

--- src/Shitpost.sol
+++ src/Shitpost.sol
@@ -5,16 +5,26 @@ import "./BBB.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Shitpost is Ownable {
-    address payable feeRecipient;
-    BBB bbb;
+    address payable public feeRecipient;
+    BBB public immutable bbb;
 
     event Post(address indexed sender, uint256 indexed tokenId, string indexed message);
-    
+    event FeeRecipientChanged(address indexed newFeeRecipient);
+
     error TokenDoesNotExist();
 
     constructor(BBB _bbb, address payable _feeRecipient, address _owner) Ownable(_owner) {
         bbb = BBB(_bbb);
         feeRecipient = _feeRecipient;
+    }
+
+    /**
+     * @notice Change the fee recipient
+     * @param newFeeRecipient The new fee recipient
+     */
+    function changeFeeRecipient(address payable newFeeRecipient) external onlyOwner {
+        feeRecipient = newFeeRecipient;
+        emit FeeRecipientChanged(newFeeRecipient);
     }
 
     /**
