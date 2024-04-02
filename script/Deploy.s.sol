1e86d77b7fc75338a949ceae21a8b7bc2e4ec12a
nicholas <nnnnicholas@users.noreply.github.com>
2024-04-02 18:27:24 +0200

    correct deploy script constructor args + comment out shitpost deploy

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -16,16 +16,20 @@ contract Deploy is Script {
     address owner = address(0xfb7C4BAA7ACfb6eBc82cdf7c814850683d549a89); // TODO CHANGE THIS!!
     uint256 protocolFeePoints = 50;
     uint256 creatorFeePoints = 50;
-    string contractJson = '{"name": "bbb","description": "bbb beautiful. bbb bold. bbb brilliant. Post for free. Earn trading fees. Have fun. Get your bbb at https://bbb.deals","image": "ipfs://QmQ8L9VD5xuFBS1rFV195Mf71E7aRsjXEU9WbchhPA9PFK","banner_image": "ipfs://QmZ443HRoAm56XvLipP2yNMe9rUATAxrXe7XRdEVU5sKeG","featured_image": "ipfs://QmdN9oxCVTTQ2mZgAYm4vTYbuQKDRJE9SUPYgKKcMZVfRt","external_link": "https://bbb.deals"}'; // TODO CHANGE THIS
+    string contractJson =
+        '{"name": "bbb","description": "bbb beautiful. bbb bold. bbb brilliant. Post for free. Earn trading fees. Have fun. Get your bbb at https://bbb.deals","image": "ipfs://QmQ8L9VD5xuFBS1rFV195Mf71E7aRsjXEU9WbchhPA9PFK","banner_image": "ipfs://QmZ443HRoAm56XvLipP2yNMe9rUATAxrXe7XRdEVU5sKeG","featured_image": "ipfs://QmdN9oxCVTTQ2mZgAYm4vTYbuQKDRJE9SUPYgKKcMZVfRt","external_link": "https://bbb.deals"}'; // TODO
+        // CHANGE THIS
 
-    function run() public returns (BBB bbb, Shitpost shitpost) {
+    function run() public returns (BBB bbb) 
+    // , Shitpost shitpost
+    {
         uint256 deployerPrivateKey = vm.envUint("BBB_DEPLOYER_PRIVATE_KEY"); // You must have this env var set on your
             // local
             // machine for a PK that has ETH at its first derivation path
             // public address 0xBBB38c30E57a9FEf6068778caB91f1a0D4948BBb
         vm.startBroadcast(deployerPrivateKey);
         // TODO set contract JSON
-        bbb = new BBB(owner, protocolFeeRecipient,protocolFeePoints, creatorFeePoints, name, version, contractJson);
+        bbb = new BBB(owner, protocolFeeRecipient, protocolFeePoints, creatorFeePoints, name, version, contractJson);
 
         // shitpost = new Shitpost(bbb, protocolFeeRecipient, moderator);
         vm.stopBroadcast();
